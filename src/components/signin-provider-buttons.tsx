159b0ca38881f1e8b548f7da41bacaf6ccc4e3c5
josuegoyret <fjgoyretsola@gmail.com>
2024-11-25 09:46:55 -0300

    feat: add providers and sign actions

--- src/components/signin-provider-buttons.tsx
+++ src/components/signin-provider-buttons.tsx
@@ -3,19 +3,24 @@
 import { Button } from "./ui/button";
 import { FaGoogle, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import {
+  signWithGitHub,
+  signWithGoogle,
+  signWithTwitter,
+} from "~/actions/auth";
 
 const SignInProviderButtons = () => {
   return (
     <div className="flex items-center gap-2">
-      <Button variant="outline" className="flex-1">
+      <Button variant="outline" className="flex-1" onClick={signWithGoogle}>
         <FaGoogle />
         Google
       </Button>
-      <Button variant="outline" className="flex-1">
+      <Button variant="outline" className="flex-1" onClick={signWithGitHub}>
         <FaGithub />
-        Github
+        GitHub
       </Button>
-      <Button variant="outline" className="flex-1">
+      <Button variant="outline" className="flex-1" onClick={signWithTwitter}>
         <FaXTwitter /> Twitter
       </Button>
     </div>
