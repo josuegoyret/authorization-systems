2ce9aae326e43b5e6c48df2f2cbf5fb46da585a9
josuegoyret <fjgoyretsola@gmail.com>
2024-11-26 11:19:47 -0300

    feat: add nodemailer provider with prisma adapter

--- auth.ts
+++ auth.ts
@@ -2,7 +2,19 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Twitter from "next-auth/providers/twitter";
+import Nodemailer from "next-auth/providers/nodemailer";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "~/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, GitHub, Twitter],
+  providers: [
+    Google,
+    GitHub,
+    Twitter,
+    Nodemailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
 });
