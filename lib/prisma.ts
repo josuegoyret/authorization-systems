e21dcaf219ae022c05b3a2bb17a5dfc0ca36ad02
josuegoyret <fjgoyretsola@gmail.com>
2024-12-10 06:50:22 -0300

    feat: configure prisma accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,26 +1,39 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
+// driver configutarion imports: -----------------
+
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaNeon } from "@prisma/adapter-neon";
+// import { Pool } from "@neondatabase/serverless";
+
+// accelerate configuration imports: -----------------
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// if not needing to be edge compatible
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
+/**
+ * // if not needing to be edge compatible
+ * export const prisma = globalForPrisma.prisma || new PrismaClient();
+ */
 
-// trun into edge compatible by adding neon database driver adapter:
-// you must the driver over HTTP, and you can't do it by setting a websocket
-let _prisma = globalForPrisma.prisma;
+/**
+ * // trun into edge compatible by adding neon database driver adapter:
+ * // you must the driver over HTTP, and you can't do it by setting a websocket
+ *
+ * let _prisma = globalForPrisma.prisma;
+ *
+ * if (!_prisma) {
+ * const connectionString = process.env.DATABASE_URL;
+ * const pool = new Pool({ connectionString });
+ * const adapter = new PrismaNeon(pool);
+ * _prisma = new PrismaClient({ adapter });
+ * }
+ */
 
-if (!_prisma) {
-  const connectionString = process.env.DATABASE_URL;
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaNeon(pool);
-  _prisma = new PrismaClient({ adapter });
-}
-
-// additionally, you can integrate prisma accelerate, which is edge compatible
+// additionally, you can integrate prisma accelerate, which is edge compatible,
 // and doesn't need a special driver to be configured
 
-export const prisma = _prisma;
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
